1129819027a9dd0db45d06569a3452e31523cb7c
Behnam Sajadifar <beh.sajadi94@gmail.com>
2022-09-30 21:41:23 +0330

    feat(shared): add file-upload component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CardComponent } from './components/card/card.component';
+import { FileUploadComponent } from './components/file-upload/file-upload.component';
 
 @NgModule({
-  declarations: [CardComponent],
+  declarations: [CardComponent, FileUploadComponent],
   imports: [CommonModule],
-  exports: [CardComponent],
+  exports: [CardComponent, FileUploadComponent],
 })
 export class SharedModule {}
