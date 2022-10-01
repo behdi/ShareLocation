3a2fff7a04ef5fc7a857d8bc1d4910c0f17f1ee4
Behnam Sajadifar <beh.sajadi94@gmail.com>
2022-10-01 11:09:31 +0330

    feat(map): add map marker and map info window

--- src/app/shared/components/map/map.component.ts
+++ src/app/shared/components/map/map.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  OnInit,
+  Output,
+  ViewChild,
+} from '@angular/core';
+import { GoogleMap, MapInfoWindow, MapMarker } from '@angular/google-maps';
 
 @Component({
   selector: 'app-map',
@@ -6,6 +13,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./map.component.less'],
 })
 export class MapComponent implements OnInit {
+  @ViewChild(GoogleMap, { static: false }) map!: GoogleMap;
+  @ViewChild(MapInfoWindow, { static: false }) infoWindow!: MapInfoWindow;
+
+  @Output() selectedPosition = new EventEmitter<google.maps.LatLngLiteral>();
+  @Output() mapClick = new EventEmitter<void>();
+
   center!: google.maps.LatLngLiteral;
   zoom = 15;
   options: google.maps.MapOptions = {
@@ -16,7 +29,10 @@ export class MapComponent implements OnInit {
     disableDoubleClickZoom: true,
     maxZoom: 18,
     minZoom: 10,
-  }
+    fullscreenControl: false,
+  };
+  markerPosition?: google.maps.LatLngLiteral;
+
   constructor() {}
 
   ngOnInit(): void {
@@ -24,5 +40,18 @@ export class MapComponent implements OnInit {
       (pos) =>
         (this.center = { lat: pos.coords.latitude, lng: pos.coords.longitude })
     );
+  }
+
+  onMapClick(event: google.maps.MapMouseEvent) {
+    this.mapClick.emit();
+  }
+
+  onMapDblClick(event: google.maps.MapMouseEvent) {
+    this.markerPosition = event.latLng?.toJSON();
+    this.selectedPosition.emit(this.markerPosition);
+  }
+
+  openInfo(marker: MapMarker) {
+    this.infoWindow.open(marker);
   }
 }
--- src/app/user-profile/pages/share-location-page/share-location-page.component.ts
+++ src/app/user-profile/pages/share-location-page/share-location-page.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-share-location-page',
   templateUrl: './share-location-page.component.html',
-  styleUrls: ['./share-location-page.component.less']
+  styleUrls: ['./share-location-page.component.less'],
 })
 export class ShareLocationPageComponent implements OnInit {
+  constructor(private router: Router) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  onMapClick() {}
+
+  getSelectedPosition(selectedPosition: google.maps.LatLngLiteral) {
+    console.log(selectedPosition);
   }
-
 }
